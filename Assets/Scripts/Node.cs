c5f6fbee49d19ca538be9b4190e9bb7a9d1ba93a
merpnderp <kalebmurphy@gmail.com>
2014-01-13 20:10:46 -0600

    just upgraded the project

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -4,138 +4,139 @@ using System.Collections;
 public class Node
 {
 
-	private int level;
-	private float width;
-	private float halfWidth;
-	private Node parent;
-	private QuadTree tree;
-	private Vector3 position;
-	public string name;
+		private int level;
+		private float width;
+		private float halfWidth;
+		private Node parent;
+		private QuadTree tree;
+		private Vector3 position;
+		public string name;
 	
-	// Children	
-	private Node topLeftChild;
-	private Node topRightChild;
-	private Node bottomLeftChild;
-	private Node bottomRightChild;
+		// Children	
+		private Node topLeftChild;
+		private Node topRightChild;
+		private Node bottomLeftChild;
+		private Node bottomRightChild;
 	
-	// Neighbors
-	public Node leftNeighbor;
-	public Node rightNeighbor;
-	public Node topNeighbor;
-	public Node bottomBeighbor;
-	public float c = 1;
-	public bool isSplit = false;
-	public bool isDrawn = false;
-	private GameObject prefab;
+		// Neighbors
+		public Node leftNeighbor;
+		public Node rightNeighbor;
+		public Node topNeighbor;
+		public Node bottomBeighbor;
+		public float c = 1;
+		public bool isSplit = false;
+		public bool isDrawn = false;
+		private GameObject prefab;
 	
-	public Node (Node _parent, int _level, QuadTree _tree, Vector3 _position, string _name)
-	{
-		name = _name;
-		parent = _parent;
-		//rootnode is level 0
-		level = _level;
-		tree = _tree;
-		position = _position;
-		width = (tree.radius * 2) / Mathf.Pow (2, level); 
-		halfWidth = width / 2;
-	}
-	
-	public void Draw ()
-	{
-		if (level == 0 || (ShouldSplit () && level < 20)) {
-			if (isDrawn) {
-				UnityEngine.Object.Destroy (prefab);
-				isDrawn = false;
-			}
-			if (! isSplit) {
-				Split ();
-			} 
-			topLeftChild.Draw ();
-			topRightChild.Draw ();
-			bottomLeftChild.Draw ();
-			bottomRightChild.Draw ();
-			
-		} else if (! isDrawn && ! isSplit) {
-			prefab = tree.sphere.GetPrefab ();
-			
-			MeshRenderer mr = (MeshRenderer)prefab.GetComponent ("MeshRenderer");
-			mr.renderer.material.SetVector ("HeightDir", tree.heightDir);
-			mr.renderer.material.SetVector ("WidthDir", tree.widthDir);
-			mr.renderer.material.SetVector ("Position", position);
-			mr.renderer.material.SetFloat ("Width", width);
-			mr.renderer.material.SetFloat ("Radius", tree.sphere.radius);
-			mr.renderer.material.SetFloat ("c", c);	
-
-			isDrawn = true;
-			Debug.Log (name + " : " + tree.widthDir + " : " + tree.heightDir + " : " + position);
+		public Node (Node _parent, int _level, QuadTree _tree, Vector3 _position, string _name)
+		{
+				name = _name;
+				parent = _parent;
+				//rootnode is level 0
+				level = _level;
+				tree = _tree;
+				position = _position;
+				width = (tree.radius * 2) / Mathf.Pow (2, level); 
+				halfWidth = width / 2;
 		}
-	}
+	
+		public void Draw ()
+		{
+				if (level == 0 || (ShouldSplit () && level < 20)) {
+						if (isDrawn) {
+								UnityEngine.Object.Destroy (prefab);
+								isDrawn = false;
+						}
+						if (! isSplit) {
+								Split ();
+						} 
+						topLeftChild.Draw ();
+						topRightChild.Draw ();
+						bottomLeftChild.Draw ();
+						bottomRightChild.Draw ();
+			
+				} else if (! isDrawn && ! isSplit) {
+						prefab = tree.sphere.GetPrefab ();
+			
+						MeshRenderer mr = (MeshRenderer)prefab.GetComponent ("MeshRenderer");
+						mr.renderer.material.SetVector ("HeightDir", tree.heightDir);
+						mr.renderer.material.SetVector ("WidthDir", tree.widthDir);
+						mr.renderer.material.SetVector ("Position", position);
+						mr.renderer.material.SetFloat ("Width", width);
+						mr.renderer.material.SetFloat ("Radius", tree.sphere.radius);
+						mr.renderer.material.SetFloat ("c", c);	
 
-	private bool ShouldSplit ()
-	{
-		if (ContainsCamera ()) {
-			float sd = (tree.sphere.splitDistance / (level + 1)) - tree.sphere.radius;
-			float td = Vector3.Distance (tree.localMatrix.MultiplyPoint (tree.sphere.playerObject.transform.position), position + (tree.widthDir + tree.heightDir) * halfWidth);
-			if (td < sd) {
+						isDrawn = true;
+						Debug.Log (name + " : " + tree.widthDir + " : " + tree.heightDir + " : " + position);
+				}
+		}
+
+		private bool ShouldSplit ()
+		{
+				if (ContainsCamera ()) {
+						float sd = (tree.sphere.splitDistance / (level + 1)) - tree.sphere.radius;
+						float td = Vector3.Distance (tree.localMatrix.MultiplyPoint (tree.sphere.playerObject.transform.position), position + (tree.widthDir + tree.heightDir) * halfWidth);
+						if (td < sd) {
+								return true;	
+						}
+				}
+				return false;
+		}
+	
+		private bool IsOccluded ()
+		{
+		
 				return true;	
-			}
 		}
-		return false;
-	}
 	
-	private bool IsOccluded(){
-		
-		return true;	
-	}
-	
-	private bool ContainsCamera ()
-	{
-		if (tree.containsCamera) {
-			if (InThisAxis (tree.cameraPoint, position, tree.heightDir, width)) {
-				if (InThisAxis (tree.cameraPoint, position, tree.widthDir, width)) {
-					return true;
-				}	
-			}
+		private bool ContainsCamera ()
+		{
+				if (tree.containsCamera) {
+						if (InThisAxis (tree.cameraPoint, position, tree.heightDir, width)) {
+								if (InThisAxis (tree.cameraPoint, position, tree.widthDir, width)) {
+										return true;
+								}	
+						}
+				}
+				return false;
 		}
-		return false;
-	}
 	
-	private bool InThisAxis (Vector3 c, Vector3 p, Vector3 axis, float w)
-	{
-		bool i = false;
-		if (axis.x == -1) {
-			i = (p.x > c.x && c.x > p.x + (w * Mathf.Sign (axis.x)));
-		} else if (axis.x == 1) {
-			i = (p.x < c.x && c.x < p.x + (w * Mathf.Sign (axis.x)));
+		private bool InThisAxis (Vector3 c, Vector3 p, Vector3 axis, float w)
+		{
+				bool i = false;
+				if (axis.x == -1) {
+						i = (p.x > c.x && c.x > p.x + (w * Mathf.Sign (axis.x)));
+				} else if (axis.x == 1) {
+						i = (p.x < c.x && c.x < p.x + (w * Mathf.Sign (axis.x)));
 			
-		} else if (axis.y == -1) {
-			i = (p.y > c.y && c.y > p.y + (w * Mathf.Sign (axis.y)));
-		} else if (axis.y == 1) {
-			i = (p.y < c.y && c.y < p.y + (w * Mathf.Sign (axis.y)));
+				} else if (axis.y == -1) {
+						i = (p.y > c.y && c.y > p.y + (w * Mathf.Sign (axis.y)));
+				} else if (axis.y == 1) {
+						i = (p.y < c.y && c.y < p.y + (w * Mathf.Sign (axis.y)));
 			
-		} else if (axis.z == -1) {
-			i = (p.z > c.z && c.z > p.z + (w * Mathf.Sign (axis.z)));
-		} else {
-			i = (p.z < c.z && c.z < p.z + (w * Mathf.Sign (axis.z)));
+				} else if (axis.z == -1) {
+						i = (p.z > c.z && c.z > p.z + (w * Mathf.Sign (axis.z)));
+				} else {
+						i = (p.z < c.z && c.z < p.z + (w * Mathf.Sign (axis.z)));
+				}
+				return i;
 		}
-		return i;
-	}
 	
-	public void Split ()
-	{
-		float halfWidth = width / 2;
-		topLeftChild = new Node (this, level + 1, tree, position + (tree.heightDir * halfWidth), "BottomLeft");
-		topLeftChild.c = 1;
+		public void Split ()
+		{
+				float halfWidth = width / 2;
+				topLeftChild = new Node (this, level + 1, tree, position + (tree.heightDir * halfWidth), "BottomLeft");
+				topLeftChild.c = 1;
 		
-		topRightChild = new Node (this, level + 1, tree, position + (tree.heightDir + tree.widthDir) * halfWidth, "BottomRight");
-		topRightChild.c = 2;
+				topRightChild = new Node (this, level + 1, tree, position + (tree.heightDir + tree.widthDir) * halfWidth, "BottomRight");
+				topRightChild.c = 2;
 		
-		bottomLeftChild = new Node (this, level + 1, tree, position, "TopLeft");
-		bottomLeftChild.c = 3;
+				bottomLeftChild = new Node (this, level + 1, tree, position, "TopLeft");
+				bottomLeftChild.c = 3;
 		
-		bottomRightChild = new Node (this, level + 1, tree, position + (tree.widthDir * halfWidth), "TopRight");
-		bottomRightChild.c = 4;
+				bottomRightChild = new Node (this, level + 1, tree, position + (tree.widthDir * halfWidth), "TopRight");
+				bottomRightChild.c = 4;
 		
-		isSplit = true;
-	}
+				isSplit = true;
+		}
 }
